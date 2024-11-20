fd1b11de7f6f3aaec8fc19d1ecfc6d8b6c229c7e
AlbinJames56 <albinjames56@gmail.com>
2024-11-20 13:49:17 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactComponent } from './contact/contact.component'; 
+import { SavedRecipesComponent } from './saved-recipes/saved-recipes.component';
+import { AboutComponent } from './about/about.component'; 
+import { RecipesComponent } from './recipes/recipes.component';
+import { ViewRecipesComponent } from './view-recipes/view-recipes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:HomeComponent},
+    {path:"login",component:LoginComponent},
+    {path:"register",component:RegisterComponent},
+    {path:"contact",component:ContactComponent},
+    {path:"recipes",component:RecipesComponent},
+    {path:"saved-recipe",component:SavedRecipesComponent},
+    {path:"about",component:AboutComponent},
+    {path:"recipes/:id/view",component:ViewRecipesComponent},
+];
