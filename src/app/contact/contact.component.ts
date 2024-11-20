6d55aa7cdfb376c6eb140a81fbdedafa211a8bfe
AlbinJames56 <albinjames56@gmail.com>
2024-11-20 16:32:09 +0530

    Created about page and contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,RouterLink],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+demoMail:String='abc@gmail.com'
 }
