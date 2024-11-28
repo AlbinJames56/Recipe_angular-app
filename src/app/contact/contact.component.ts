4c664d32ec6da35806eadc5ac0847292bf230c6d
AlbinJames56 <albinjames56@gmail.com>
2024-11-28 15:34:20 +0530

    created testimonilas, and enable filter and search

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
+import { HeaderComponent } from '../header/header.component';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { emit } from 'process';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [HeaderComponent,RouterLink],
+  imports: [HeaderComponent, RouterLink, ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
-demoMail:String='abc@gmail.com'
+  demoMail: String = 'abc@gmail.com';
+  testimonyForm: FormGroup;
+  constructor(private fb: FormBuilder, private api: ApiService) {
+    this.testimonyForm = this.fb.group({
+      name: [''],
+      email: [''],
+      message: [''],
+    });
+  }
+
+  submitTestimony() {
+    const name = this.testimonyForm.value.name;
+    const email = this.testimonyForm.value.email;
+    const message = this.testimonyForm.value.message;
+    if (name && email && message) {
+      this.api
+        .saveTestimonialsAPI({ name, email, message })
+        .subscribe((res: any) => {
+          alert('Thankyou for your testimony');
+          this.testimonyForm.reset();
+        });
+    } else {
+      alert('Please fill missing fields');
+    }
+  }
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { ApiService } from '../services/api.service';
+ 
+import { SearchPipe } from '../pipes/search.pipe';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,SearchPipe,FormsModule,NgxPaginationModule],
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.css'
+  styleUrl: './recipes.component.css',
 })
 export class RecipesComponent {
+  allRecipes: any = [];
+  allRecipesDummy: any = [];
+  searchKey:string=""
+  constructor(private api: ApiService) {}
+  p:number=1;
+  ngOnInit() {
+    this.getAllRecipes();
+  }
 
+  getAllRecipes() {
+    this.api.getAllRecipeAPI().subscribe((res: any) => {
+      this.allRecipes = res;
+      this.allRecipesDummy = this.allRecipes;
+      console.log(this.allRecipes);
+    });
+  }
+  filterRecipes(recipeType: string, recipeName: string) {
+    this.allRecipes = this.allRecipesDummy.filter((item: any) =>
+      item[recipeType]?.includes(recipeName)
+    );
+  }
 }
